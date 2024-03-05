5e7b47b3d156eb4d7650f0f461731a2c12d52e83
Teddy Pascual <teddylayson2000@gmail.com>
2024-03-05 10:10:00 +0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-800 text-white min-h-screen">
+      <h1 className="text-3xl text-center">
+        Websocket Practice with React and Express
+      </h1>
+    </div>
+  );
+}
+
+export default App;
