b8c66861c9ab7de9bed6eeb46b46374a84e4c949
Teddy Pascual <teddylayson2000@gmail.com>
2024-03-06 20:37:18 +0800

    fix bug when joining a room with no state

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import MessageForm from "../components/MessageForm";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 const Room = () => {
   const navigate = useNavigate();
+
   const { state } = useLocation();
+  useEffect(() => {
+    if (state === null || "" || undefined) {
+      navigate("/");
+    }
+  }, []);
   if (state === null || "" || undefined) {
     return <h1>Unauthorized</h1>;
   } else {
