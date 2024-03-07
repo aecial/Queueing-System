6d3fb8e7a02a2667d4ed0aa30a7090f26018e99f
Teddy Pascual <teddylayson2000@gmail.com>
2024-03-07 19:34:55 +0800

    add dynamic mapping of rooms

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,26 +2,22 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
+  const rooms = [1, 2, 3, 4, 5];
   return (
     <div className="bg-gray-800 text-white min-h-screen">
-      <div className="border border-white p-2">
-        <h3>Room 1</h3>
-        <button
-          onClick={() => navigate("/room", { state: { room: 1 } })}
-          className="m-4 border border-white p-1"
-        >
-          Join Room
-        </button>
-      </div>
-      <div className="border border-white p-2">
-        <h3>Room 2</h3>
-        <button
-          onClick={() => navigate("/room", { state: { room: 2 } })}
-          className="m-4 border border-white p-1"
-        >
-          Join Room
-        </button>
-      </div>
+      {rooms.map((room) => {
+        return (
+          <div className="border border-white p-2">
+            <h3>Room {room}</h3>
+            <button
+              onClick={() => navigate("/room", { state: { room } })}
+              className="m-4 border border-white p-1"
+            >
+              Join Room {room}
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
