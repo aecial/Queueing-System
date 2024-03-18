534563d79141bb557c8f9570cd897265e0a05637
Teddy Pascual <teddylayson2000@gmail.com>
2024-03-18 22:59:22 +0800

    add dynamic redirect to department watcher

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
-  const rooms = [1, 2, 3, 4, 5];
+  const [departments, setDepartments] = useState([]);
+  async function getDepartments() {
+    const response = await fetch(`http://localhost:8080/departments`);
+    const departments = await response.json();
+    setDepartments(departments.departments);
+  }
+  useEffect(() => {
+    getDepartments();
+  }, []);
   return (
     <div className="bg-gray-800 text-white min-h-screen">
-      {rooms.map((room) => {
+      {/* {rooms.map((room) => {
         return (
           <div className="border border-white p-2">
             <h3>Room {room}</h3>
@@ -14,6 +22,19 @@ const Home = () => {
               className="m-4 border border-white p-1"
             >
               Join Room {room}
+            </button>
+          </div>
+        );
+      })} */}
+      {departments.map((department) => {
+        return (
+          <div className="border border-white p-2">
+            <h3>{department.name}</h3>
+            <button
+              onClick={() => navigate(`/watcher/${department.id}`)}
+              className="m-4 border border-white p-1"
+            >
+              {department.name} Watcher
             </button>
           </div>
         );
