e3b134c17f6eafb17f232a6e19bdb97777be9e8b
Teddy Pascual <teddylayson2000@gmail.com>
2024-04-27 17:39:56 +0800

    add base serving page

---
+++ client/src/pages/Serving.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Serving = () => {
+  return (
+    <div className="text-white min-w-screen min-h-screen flex flex-col justify-center items-center">
+      NOW SERVING
+    </div>
+  );
+};
+
+export default Serving;
